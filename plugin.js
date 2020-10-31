896cfb626b673ce0edfedb13fb503320b7afd11c
Florian Dreier <dreier@cqse.eu>
2020-10-31 16:13:20 +0100

    Fix transitive import change des not cause refresh

--- plugin.js
+++ plugin.js
@@ -22,15 +22,39 @@ const render = async (filepath, options) => {
 };
 
 module.exports = function plugin(snowpackConfig, options = {}) {
+  /** A map of imported paths to the files that imported them. */
+  const importedByMap = new Map();
+
+  function addImportsToMap(filePath, importedPath) {
+    const importedBy = importedByMap.get(importedPath);
+    if (importedBy) {
+      importedBy.add(filePath);
+    } else {
+      importedByMap.set(importedPath, new Set([filePath]));
+    }
+  }
+
   return {
     name: pkg.name,
     resolve: {
       input: ['.less'],
       output: ['.css'],
     },
-    async load({ filePath }) {
+    onChange({ filePath }) {
+      if (importedByMap.has(filePath)) {
+        const importedBy = importedByMap.get(filePath);
+        importedByMap.delete(filePath);
+        for (const importerFilePath of importedBy) {
+          this.markChanged(importerFilePath);
+        }
+      }
+    },
+    async load({ filePath, isDev }) {
       try {
         const result = await render(filePath, options);
+        if (isDev) {
+          result.imports.forEach(importedPath => addImportsToMap(filePath, importedPath));
+        }
         return {
           '.css': result.css,
         };
